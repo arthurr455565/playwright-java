163fa46c3a368b5658bc0d156e81f6aa3b489b51
arthurr455565 <bishalroy909@gmail.com>
2026-07-17 13:43:36 +0545

    Added Amazon NNavigator and Readme file

---
+++ src/main/java/org/example/Amazon_Search.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Google_Search {
+
+    public static void main(String[] args) {
+
+        // Create Playwright instance
+        try (Playwright playwright = Playwright.create()) {
+
+            // Launch Chromium browser
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions()
+                            .setHeadless(false)
+            );
+
+            // Create a new browser context
+            BrowserContext context = browser.newContext();
+
+            // Create a new page (tab)
+            Page page = context.newPage();
+
+            // Navigate to Google
+            page.navigate("https://www.amazon.in");
+
+            Locator search = page.locator("#twotabsearchtextbox");
+            search.fill("Iphone");
+            search.press("Enter");
+
+            // Wait for 5 seconds
+            page.waitForTimeout(5000);
+
+            // Close browser
+            browser.close();
+        }
+    }
+}
